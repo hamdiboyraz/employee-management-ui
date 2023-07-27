9d67d5ef58d50c535c30ce826776ca34475db75e
hamdiboyraz <hamdiboyraz19@gmail.com>
2023-07-28 01:59:29 +0300

    Complete project

--- src/components/UpdateEmployee.js
+++ src/components/UpdateEmployee.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import EmployeeService from "../services/EmployeeService";
 
 const initialFormState = {
@@ -9,10 +9,10 @@ const initialFormState = {
   emailId: "",
 };
 
-const AddEmployee = () => {
-  const [employee, setEmployee] = useState(initialFormState);
-
+const UpdateEmployee = () => {
+  const { id } = useParams();
   const navigate = useNavigate();
+  const [employee, setEmployee] = useState(initialFormState);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -22,25 +22,33 @@ const AddEmployee = () => {
     }));
   };
 
-  const addEmployee = async () => {
+  const updateEmployee = async () => {
     try {
-      const response = await EmployeeService.createEmployee(employee);
+      const response = await EmployeeService.updateEmployee(id, employee);
       console.log(response);
-      navigate("/employeelist");
+      navigate("/employeeList");
     } catch (error) {
       console.log(error);
     }
   };
 
-  const reset = () => {
-    setEmployee(initialFormState);
+  const fetchData = async () => {
+    try {
+      const response = await EmployeeService.getEmployeeById(id);
+      setEmployee(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <div className="flex max-w-2xl mx-auto shadow border-b">
       <div className="px-8 py-8">
         <div className="font-thin text-2xl tracking-wider">
-          <h1>Add New Employee</h1>
+          <h1>Update Employee</h1>
         </div>
         <div className="items-center justify-center h-14 w-full my-4">
           <label className="block text-gray-600 text-sm font-normal">
@@ -81,16 +89,10 @@ const AddEmployee = () => {
 
         <div className="items-center justify-center h-14 w-full my-4 space-x-4 pt-4">
           <button
-            onClick={addEmployee}
+            onClick={updateEmployee}
             className="rounded text-white font-semibold bg-green-400 hover:bg-green-700 py-2 px-6"
           >
-            Save
-          </button>
-          <button
-            onClick={reset}
-            className="rounded text-white font-semibold bg-gray-400 hover:bg-gray-700 py-2 px-6"
-          >
-            Clear
+            Update
           </button>
           <button
             onClick={() => navigate("/employeeList")}
@@ -104,4 +106,4 @@ const AddEmployee = () => {
   );
 };
 
-export default AddEmployee;
+export default UpdateEmployee;
--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -6,6 +6,25 @@ class EmployeeService {
   createEmployee(employee) {
     return axios.post(EMPLOYEE_API_BASE_URL, employee);
   }
+
+  getEmployees() {
+    return axios.get(EMPLOYEE_API_BASE_URL);
+  }
+
+  deleteEmployee(id) {
+    return axios.delete(`${EMPLOYEE_API_BASE_URL}/${id}`);
+  }
+
+  getEmployeeById(id) {
+    return axios.get(`${EMPLOYEE_API_BASE_URL}/${id}`);
+  }
+
+  updateEmployee(id, employee) {
+    return axios.put(`${EMPLOYEE_API_BASE_URL}/${id}`, employee);
+  }
 }
 
-export default new EmployeeService();
+const employeeServiceInstance = new EmployeeService();
+
+export default employeeServiceInstance;
+// export default new EmployeeService();
